f620e358e1a1a81eff8cac65956fd636db5323c7
OfisHelper <124517979+OfisHelper@users.noreply.github.com>
2023-04-25 22:17:11 +0200

    Update synthétiseur.js

--- synthétiseur.js
+++ synthétiseur.js
@@ -50,9 +50,8 @@ app.post('/generate', (req, res) => {
 
 const response_text = `<center><h2 style="::selection {
     background-color: #000000;
-    color: #ffffff;
-}
-;font-weight: 600; font-size: 3vw; color:white;">Résultat :</h2></center><br><div style="color:white; font-size: 20px;">${result}</div>`;
+    color: black;
+};font-weight: 600; font-size: 3vw; color:white;">Résultat :</h2></center><br><div style="color:white; font-size: 20px;">${result}</div>`;
       console.log(body.choices[0])
       res.send(response_text);
       console.log(response_text )
