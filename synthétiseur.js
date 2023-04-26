2ae86fd2af49a08b4da578f591cc3b25845173e1
OfisHelper <124517979+OfisHelper@users.noreply.github.com>
2023-04-26 18:54:16 +0200

    Update synthétiseur.js

--- synthétiseur.js
+++ synthétiseur.js
@@ -54,31 +54,6 @@ app.post('/generate', (req, res) => {
     });
 });
 
-// Récupération du bouton de copie
-const copyButton = document.querySelector(".copy-button");
-
-// Ajout d'un écouteur d'événement pour le clic sur le bouton de copie
-copyButton.addEventListener("click", () => {
-    // Récupération du résultat synthétisé
-    const result = document.querySelector("#result").innerHTML;
-    
-    // Envoi d'une requête POST pour copier le texte en noir dans le presse-papiers
-    fetch("/copy", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({result: result})
-    }).then(() => {
-        // Affichage d'un message de confirmation de la copie
-        alert("Le texte a été copié dans le presse-papiers !");
-    }).catch((error) => {
-        console.error(error);
-        alert("Erreur lors de la copie du texte : " + error.message);
-    });
-});
-
-
     res.sendStatus(200);
 });
 
