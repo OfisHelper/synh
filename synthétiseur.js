311a8127293c96c5c15734ef0f9543a4a3e440b9
OfisHelper <124517979+OfisHelper@users.noreply.github.com>
2023-04-26 17:19:20 +0200

    Update synthétiseur.js

--- synthétiseur.js
+++ synthétiseur.js
@@ -45,27 +45,33 @@ app.post('/generate', (req, res) => {
       }
   
       const result = body.choices[0].message.content.split('\n').map(line => {
-          return `<p>${line}</p>`;
+        return `<p>${line}</p>`;
       }).join('');
-      
-      const response_text = `<center><h2 class="copy" style="font-weight: 600; font-size: 3vw; color:white;">Résultat :</h2></center><br><div class="copy" style="color:white; font-size: 20px;" oncopy="onCopy(event)">${result}</div><span class="copied" style="position:absolute; right:-9999px"></span>`;
+
+      const response_text = `<center><h2 class="copy" style="font-weight: 600; font-size: 3vw; color:white;">Résultat :</h2></center><br><div style="color:white; font-size: 20px;">${result}</div>`;
       console.log(body.choices[0])
       res.send(response_text);
       console.log(response_text )
-    });
+
+      // Ajouter le texte à l'élément invisible dans la page pour pouvoir le copier
+      const hiddenDiv = document.createElement('div');
+      hiddenDiv.innerHTML = result.replace(/<p>/g, '').replace(/<\/p>/g, '\n');
+      hiddenDiv.style.position = 'absolute';
+      hiddenDiv.style.top = '-9999px';
+      document.body.appendChild(hiddenDiv);
+
+      // Sélectionner et copier le texte en noir
+      const range = document.createRange();
+      range.selectNode(hiddenDiv);
+      window.getSelection().removeAllRanges();
+      window.getSelection().addRange(range);
+      document.execCommand('copy');
+      window.getSelection().removeAllRanges();
+
+      // Retirer l'élément invisible de la page
+      document.body.removeChild(hiddenDiv);
+  });
   
 });
 
-function onCopy(event) {
-    event.preventDefault();
-    var text = window.getSelection().toString();
-    event.clipboardData.setData("text/plain", text);
-    event.clipboardData.setData("text/html", text);
-    document.querySelector(".copied").innerHTML = "Copié !";
-    setTimeout(() => {
-        document.querySelector(".copied").innerHTML = "";
-    }, 1500);
-}
-
 app.listen(process.env.PORT || port, () => console.log('Listening on port 3000'));
-
