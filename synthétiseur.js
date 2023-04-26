cfd4d5d689990a01e1d9e4214530e2b59c2ab050
OfisHelper <124517979+OfisHelper@users.noreply.github.com>
2023-04-26 19:38:04 +0200

    Update synthétiseur.js

--- synthétiseur.js
+++ synthétiseur.js
@@ -1,50 +1,70 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>SynHelper - OfisHelper | GPT-3.5-Turbo API </title>
-    <link rel="stylesheet" href="style.css">
-  </head>
-  <body>
-    <center>
-    <h2>Synthétiseur de Texte</h2>
-    <form action="/generate" method="post">
-      <h3 for="article_text"></h3>
-      <textarea style="resize: none;" required="required" placeholder="Entrez ici le texte a Synthétiser..." rows="30" name="article_text" id="article_text" cols="60" class="ui-autocomplete-input" autocomplete="off" role="textbox" aria-autocomplete="list" aria-haspopup="true"></textarea>
-      <h3>Longueur du texte final :</h3>
-      <div>
-        <label class="rad-label">
-          <input type="radio" class="rad-input" id="rapide" name="style" value="Rapide">
-          <div class="rad-design"></div>
-          <div class="rad-text">Court</div>
-        </label>
+const express = require('express');
+const app = express();
+const request = require('request');
+const path = require('path');
+
+const OPENAI_API_KEY = process.env.TOKEN ; // à remplacer par votre clé API OpenAI
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.post('/generate', (req, res) => {
+    const article_text = req.body.article_text;
+    const style = req.body.style;
+
+    const options = {
+        url: 'https://api.openai.com/v1/chat/completions',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${OPENAI_API_KEY}`
+        },
+        json: {
+            messages: [
+              {"role": "system", "content": "Tu sert seulement à synthétiser des textes"},
+              {"role": "user", "content": article_text + "de manière" + style} ,
+          ],
+            model : "gpt-3.5-turbo",
+            max_tokens: 500,
+            temperature: 0.5
+        }
+    };
+
+    request(options, (err, response, body) => {
+      if (err) {
+          return res.send('Error generating response');
+      }
+  
+      if (body.error) {
+          return res.send(`OpenAI API error: ${body.error.message}`);
+      }
+  
+      const result = body.choices[0].message.content.split('\n').map(line => {
+          return `<p>${line}</p>`;
+      }).join('');
       
-        <label class="rad-label">
-          <input type="radio" class="rad-input" id="moyen" name="style" value="Moyen">
-          <div class="rad-design"></div>
-          <div class="rad-text">Moyen</div>
-        </label>
-      
-        <label class="rad-label">
-          <input type="radio" class="rad-input" id="long" name="style" value="Long">
-          <div class="rad-design"></div>
-          <div class="rad-text">Long</div>
-        </label>
-      </div>
-      <br><br><br>
-      <button id="generate-button" type="submit">Synthétiser le texte</button>
-      <div id="loader"></div>
-      <script>
-        const generateButton = document.querySelector("#generate-button");
-        const loader = document.querySelector("#loader");
-        generateButton.addEventListener("click", () => {
-          generateButton.style.display = "none";
-          loader.style.display = "inline-block";
-        });
-      </script>
-    </form>
-    <div id="result"></div>
-    <script src="script.js"></script>
-    </center>
-  </body>
-</html>
+      const response_text = `<center><h2 class="copy" style="font-weight: 600; font-size: 3vw; color:white;">Résultat :</h2></center><br><div class="copy" style="color:white; font-size: 20px;" oncopy="onCopy(event)">${result}</div><span class="copied" style="position:absolute; right:-9999px"></span>`;
+      console.log(body.choices[0])
+      res.send(response_text);
+      console.log(response_text )
+    });
+  
+});
+
+function onCopy(event) {
+    event.preventDefault();
+    var text = window.getSelection().toString();
+    event.clipboardData.setData("text/plain", text);
+    event.clipboardData.setData("text/html", text);
+    document.querySelector(".copied").innerHTML = "Copié !";
+    setTimeout(() => {
+        document.querySelector(".copied").innerHTML = "";
+    }, 1500);
+}
+
+app.listen(process.env.PORT || port, () => console.log('Listening on port 3000'));
