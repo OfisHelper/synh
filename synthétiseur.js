2f9f2fe610846068313047c8ec2534372a3bfd6c
OfisHelper <124517979+OfisHelper@users.noreply.github.com>
2023-04-28 22:17:01 +0200

    Update synthétiseur.js

--- synthétiseur.js
+++ synthétiseur.js
@@ -49,7 +49,7 @@ app.post('/generate', (req, res) => {
         }).join('');
         
 
-        const response_text = `<script>function copyText() {const textToCopy = document.getElementById('myDiv').innerText;const tempInput = document.createElement('input');tempInput.value = textToCopy;document.body.appendChild(tempInput);tempInput.select();document.execCommand('copy');document.body.removeChild(tempInput);}</script><center><h2 style="font-weight:600;font-size:3vw;color:white;">Résultat :</h2> <br> <div id="myDiv" style="font-size: 20px; color: white;">${result}</div><br><button onclick="copyText()">Copier le texte</button></center>`;
+        const response_text = `<script>function copyText() {const textToCopy = document.getElementById('myDiv').innerText;const tempInput = document.createElement('input');tempInput.value = textToCopy;document.body.appendChild(tempInput);tempInput.select();document.execCommand('copy');document.body.removeChild(tempInput);}</script><center><h2 style="font-weight:600;font-size:3vw;color:white;">Résultat :</h2> <br> <div id="myDiv" style="font-size: 20px; color: white;">${result}</div><br><button class="copy-button" onclick="copyText()">Copier le texte</button></center>`;
         console.log(body.choices[0])
         res.send(response_text);
         console.log(response_text );
